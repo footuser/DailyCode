83ebb7ba098ef22da8022cfff32f1ce70ba60e52
zhihuiqiu <zhihuiqiu@sohu-inc.com>
2014-12-10 10:37:17 +0800

    add spark word count

--- src/main/java/spark/zhihui/SparkTest.java
+++ src/main/java/spark/zhihui/SparkTest.java
@@ -4,10 +4,15 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.storage.StorageLevel;
+
+import scala.Tuple2;
 
 public class SparkTest {
 
@@ -18,9 +23,10 @@ public class SparkTest {
         SparkConf conf = new SparkConf().setAppName("test-spark").setMaster("local[4]");
         JavaSparkContext sc = new JavaSparkContext(conf);
         List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
-        JavaRDD<Integer> distData = sc.parallelize(data);
-        
-        distData =  distData.map(new Function<Integer, Integer>() {
+        JavaRDD<Integer> originalDatas = sc.parallelize(data);
+
+        //start
+        JavaRDD<Integer> afterMapDatas = originalDatas.map(new Function<Integer, Integer>() {
 
             /**
              * 
@@ -29,11 +35,13 @@ public class SparkTest {
 
             @Override
             public Integer call(Integer in) throws Exception {
-                return in ^ 2;
+                return in;
             }
         });
+        
+//        afterMapDatas.persist(StorageLevel.DISK_ONLY());
 
-        Integer result = distData.reduce(new Function2<Integer, Integer, Integer>() {
+        Integer result = afterMapDatas.reduce(new Function2<Integer, Integer, Integer>() {
             /**
              * 
              */
@@ -44,8 +52,36 @@ public class SparkTest {
                 return a + b;
             }
         });
-        
+
         System.out.println(result);
+        
+//        afterMapDatas.unpersist();
+        
+        
+        
+        //start
+//        JavaPairRDD<String, Integer> aftermapPairs = originalDatas.mapToPair(new PairFunction<Integer, String, Integer>() {
+//
+//            /**
+//             * 
+//             */
+//            private static final long serialVersionUID = 1L;
+//
+//            @Override
+//            public Tuple2<String, Integer> call(Integer in) throws Exception {
+//                return new Tuple2<String, Integer>(in + "", 1);
+//            }
+//        });
+//        JavaPairRDD<String, Integer> resultPair = aftermapPairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+//
+//            private static final long serialVersionUID = 1L;
+//            @Override
+//            public Integer call(Integer in1, Integer in2) throws Exception {
+//                return in1 + in2;
+//            }
+//        });
+//        resultPair.collect();
+        
 
     }
 
